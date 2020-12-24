26e13e6cee3e5e2e90684ede1aa75cca82139181
japiirainen <joona.piirainen@gmail.com>
2020-12-24 22:58:41 +0200

    prettier

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { SampleButton } from './styled';
+import React from 'react'
+import { SampleButton } from './styled'
 
 export const Button: React.FC<{
-	value: string;
-	onClickHandler: () => void;
+	value: string
+	onClickHandler: () => void
 }> = ({ value, onClickHandler }) => (
 	<SampleButton onClick={onClickHandler}>{value}</SampleButton>
-);
+)
