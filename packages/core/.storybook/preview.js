7e1c31179f1e89372ab113fb16309edcfb353aa9
japiirainen <joona.piirainen@gmail.com>
2021-01-16 22:02:19 +0200

    chance name

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -25,7 +25,7 @@ addDecorator(
 addParameters({
 	options: {
 		brandTitle: 'Awesome component library',
-		brandUrl: 'https://github.com/japiirainen/-name-here-',
+		brandUrl: 'https://github.com/japiirainen/react-jp',
 		showRoots: true,
 	},
 })
